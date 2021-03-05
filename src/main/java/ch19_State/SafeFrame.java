a9e2171bcf4cbbf4fc18a17ac75595b3a7434cb6
Joseph Johnston <1193347519@qq.com>
2021-03-06 00:01:19 +0800

    第19章教材代码

--- src/main/java/ch19_State/SafeFrame.java
+++ src/main/java/ch19_State/SafeFrame.java
@@ -1,9 +1,6 @@
 package ch19_State;
 
-import java.awt.Button;
-import java.awt.Frame;
-import java.awt.TextArea;
-import java.awt.TextField;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -21,28 +18,76 @@ public class SafeFrame extends Frame implements ActionListener, Context {
 
   private State state = DayState.getInstance();
 
+  public SafeFrame(String title) {
+    super(title);
+    setBackground(Color.lightGray);
+    setLayout(new BorderLayout());
+
+    add(textClock, BorderLayout.NORTH);
+    textClock.setEditable(false);
+
+    add(textScreen, BorderLayout.CENTER);
+    textScreen.setEditable(false);
+
+    Panel panel = new Panel();
+    panel.add(buttonUse);
+    panel.add(buttonAlarm);
+    panel.add(buttonPhone);
+    panel.add(buttonExit);
+
+    add(panel, BorderLayout.SOUTH);
+
+    pack();
+    setVisible(true);
+
+    buttonUse.addActionListener(this);
+    buttonAlarm.addActionListener(this);
+    buttonPhone.addActionListener(this);
+    buttonExit.addActionListener(this);
+  }
+
   @Override
   public void setClock(int hour) {
-
+    String clockString = "现在时间是";
+    if (hour < 10) {
+      clockString += "0" + hour + ":00";
+    } else {
+      clockString += hour + ":00";
+    }
+    System.out.println(clockString);
+    textClock.setText(clockString);
+    state.doClock(this, hour);
   }
 
   @Override
   public void changeState(State state) {
-
+    System.out.println("从 " + this.state + " 状态变为了 " + state + " 状态。");
+    this.state = state;
   }
 
   @Override
   public void callSecurityCenter(String msg) {
-
+    textScreen.append("call! " + msg + "\n");
   }
 
   @Override
   public void recordLog(String msg) {
-
+    textScreen.append("record ... " + msg + "\n");
   }
 
   @Override
   public void actionPerformed(ActionEvent e) {
-
+    System.out.println(e.toString());
+    if (e.getSource() == buttonUse) {
+      state.doUse(this);
+    } else if (e.getSource() == buttonAlarm) {
+      state.doAlarm(this);
+    } else if (e.getSource() == buttonPhone) {
+      state.doPhone(this);
+    } else if (e.getSource() == buttonExit) {
+      System.exit(0);
+    } else {
+      System.out.println("?");
+    }
   }
 }

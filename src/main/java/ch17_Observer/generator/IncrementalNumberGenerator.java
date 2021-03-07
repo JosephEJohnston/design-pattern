04fc4623029fb608a19ebc35a2bd718682511ab3
Joseph Johnston <1193347519@qq.com>
2021-03-07 23:26:40 +0800

    第十七章部分练习

---
+++ src/main/java/ch17_Observer/generator/IncrementalNumberGenerator.java
@@ -0,0 +1,19 @@
+package ch17_Observer.generator;
+
+public class IncrementalNumberGenerator extends NumberGenerator {
+    private int start;
+    private int end;
+    private int step;
+    private int current;
+
+
+    @Override
+    public int getNumber() {
+        return 0;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}

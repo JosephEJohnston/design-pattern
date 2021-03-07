a683466e88370eecac490c2549c30ac89ed38b07
Joseph Johnston <1193347519@qq.com>
2021-03-07 23:19:51 +0800

    第二十章教材

---
+++ src/main/java/ch20_Flyweight/BigChar.java
@@ -0,0 +1,33 @@
+package ch20_Flyweight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BigChar {
+    private char charName;
+    private String fontData;
+
+    public BigChar(char charName) {
+        this.charName = charName;
+        try {
+            BufferedReader reader = new BufferedReader(
+                new FileReader("big" + charName + ".txt")
+            );
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append("\n");
+            }
+            reader.close();
+            this.fontData = builder.toString();
+        } catch (IOException e) {
+            this.fontData = charName + "?";
+        }
+    }
+
+    public void print() {
+        System.out.print(fontData);
+    }
+}
---
+++ src/main/java/ch20_Flyweight/BigString.java
@@ -0,0 +1,19 @@
+package ch20_Flyweight;
+
+public class BigString {
+    private BigChar[] bigChars;
+
+    public BigString(String string) {
+        bigChars = new BigChar[string.length()];
+        BigCharFactory factory = BigCharFactory.getInstance();
+        for (int i = 0; i < bigChars.length; i++) {
+            bigChars[i] = factory.getBigChar(string.charAt(i));
+        }
+    }
+
+    public void print() {
+        for (BigChar bigChar : bigChars) {
+            bigChar.print();
+        }
+    }
+}

e08dce5882ffb15cd7fbd304565c6dc274152202
wangzeyuan <wangzeyuan@nowcoder.com>
2021-03-11 11:00:33 +0800

    第二十一章教材部分

---
+++ src/main/java/ch22_Command/Command.java
@@ -0,0 +1,9 @@
+package ch22_Command;
+
+/**
+ * @author Zeyuan Wang[wangzeyuan@nowcoder.com]
+ * @date 2021/03/11
+ */
+public interface Command {
+  void execute();
+}
---
+++ src/main/java/ch22_Command/MacroCommand.java
@@ -0,0 +1,35 @@
+package ch22_Command;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * @author Zeyuan Wang[wangzeyuan@nowcoder.com]
+ * @date 2021/03/11
+ */
+public class MacroCommand implements Command {
+  private final Deque<Command> commands = new LinkedList<>();
+
+  @Override
+  public void execute() {
+    for (Command command : commands) {
+      command.execute();
+    }
+  }
+
+  public void append(Command cmd) {
+    if (cmd != this) {
+      commands.push(cmd);
+    }
+  }
+
+  public void undo() {
+    if (!commands.isEmpty()) {
+      commands.pop();
+    }
+  }
+
+  public void clear() {
+    commands.clear();
+  }
+}
